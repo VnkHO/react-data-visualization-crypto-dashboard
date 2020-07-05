cec29a06c7b440af7d3d82520e0c991a01925347
VnkHO <ho.vincent.nk@gmail.com>
2020-07-05 16:46:21 +0200

    fetch prices

--- src/Component/Shared/Content/index.js
+++ src/Component/Shared/Content/index.js
@@ -5,9 +5,12 @@ import {AppContext} from '../../../Provider/AppProvider'
 export default function ({children}) {
   return (
     <AppContext.Consumer>
-      {({coinList}) => {
+      {({coinList, prices, firstVisit}) => {
         if (!coinList) {
           return <div>Loading Coins...</div>
+        }
+        if (!firstVisit && !prices) {
+          return <div>Loading Prices ...</div>
         }
         return <div>{children}</div>
       }}
