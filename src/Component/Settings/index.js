9b13ecc3192b875eff274ec75ad56e5bf8edddb5
VnkHO <ho.vincent.nk@gmail.com>
2020-07-05 14:25:22 +0200

    page compo

--- src/Component/Settings/index.js
+++ src/Component/Settings/index.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import WelcomeMessage from '../WelcomeMessage'
 import ConfirmButton from '../ConfirmButton'
+import Page from '../Shared/Page'
 
 const Settings = () => {
   return (
-    <>
+    <Page name={'settings'}>
       <WelcomeMessage />
       <ConfirmButton />
-    </>
+    </Page>
   )
 }
 
