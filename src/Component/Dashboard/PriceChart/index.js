333a9d4497747c2eac584f058def1e6ab6fcb654
VnkHO <ho.vincent.nk@gmail.com>
2020-07-05 19:59:35 +0200

    high charts theme

--- src/Component/Dashboard/PriceChart/index.js
+++ src/Component/Dashboard/PriceChart/index.js
@@ -3,6 +3,9 @@ import {Tile} from '../../Shared/Tile'
 import {AppContext} from '../../../Provider/AppProvider'
 import ReactHighcharts from 'react-highcharts'
 import highchartsConfig from '../HighchartsConfig'
+import HighchartsTheme from '../HighchartsTheme'
+
+ReactHighcharts.Highcharts.setOptions(HighchartsTheme)
 
 export default function () {
   return (
