656ca8a185afb2ea1c881516af0993fd0f4653fa
VnkHO <ho.vincent.nk@gmail.com>
2020-07-05 15:26:25 +0200

    Coin tile

--- src/Component/Settings/CoinGrid/index.js
+++ src/Component/Settings/CoinGrid/index.js
@@ -2,19 +2,26 @@ import React from 'react'
 import styled, {css} from 'styled-components'
 import {AppContext} from '../../../Provider/AppProvider'
 import {SelectableTile} from '../../Shared/Tile'
+import CoinTile from '../CoinTile'
 
 export const CoinGridStyled = styled.div`
   display: grid;
   grid-template-columns: repeat(5, 1fr);
   grid-gap: 1.5rem;
+  margin-top: 4rem;
 `
+
+function getCoinsToDisplay(coinList) {
+  return Object.keys(coinList).slice(0, 100)
+}
+
 export default function () {
   return (
     <AppContext.Consumer>
       {({coinList}) => (
         <CoinGridStyled>
-          {Object.keys(coinList).map((coinKey) => (
-            <SelectableTile>{coinKey}</SelectableTile>
+          {getCoinsToDisplay(coinList).map((coinKey, index) => (
+            <CoinTile coinKey={coinKey} />
           ))}
         </CoinGridStyled>
       )}
